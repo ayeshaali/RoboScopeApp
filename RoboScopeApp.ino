645398df0dc583d81e26b88f8cd9b240e0fbfe4e
ayeshaali <aliayeshanyc@gmail.com>
2020-04-07 14:08:18 -0400

    flask app

--- RoboScopeApp.ino
+++ RoboScopeApp.ino
@@ -1,32 +1,67 @@
-int incomingData;
+#define START 0  //example definition
+#define DOWN 1  //example definition
+#define UP 2
+
+int down_initial;
+int up_initial;
+const int BUTTON_TIMEOUT = 150; //button timeout in milliseconds
+bool LED = 0;
+bool prev_LED = 0;
+int state = 0;
 
 void setup() {
   Serial.begin(9600); // set the baud rate
-  Serial.println("Ready"); // print "Ready" once
   pinMode(11,INPUT_PULLUP);
   pinMode(13,OUTPUT);
+  digitalWrite(13, HIGH);
 }
 
 void loop() {
-  if (Serial.available()) {
-    incomingData = Serial.read();
-    if (incomingData == '1') {
-      digitalWrite(13, HIGH);
-      Serial.write("LED Turned ON");
-    }
-    if (incomingData == '0') {
-      digitalWrite(13, LOW);
-      Serial.write("LED Turned OFF");
-    }
-  }
+  number_fsm(digitalRead(11));
+}
 
-  int val = digitalRead(11);
-  if (val){
-    digitalWrite(13, LOW);
-    Serial.println("LED Turned OFF");
-  }else{ 
-    digitalWrite(13, HIGH);
-    Serial.println("LED Turned ON");
-  }
 
+void number_fsm(uint8_t input){
+  switch(state){
+    case START: 
+      if (Serial.available()) {
+        int incomingData = Serial.read();
+        if (incomingData == '2') {
+          LED=!LED;
+        }
+      }
+
+      if (LED!=prev_LED) {
+        digitalWrite(13, LED);
+        prev_LED=LED;
+      }
+      
+      if (input==0){
+        down_initial = millis();
+        state = DOWN;
+      }
+      
+      break;
+    case DOWN:
+      if ((millis()-down_initial)< BUTTON_TIMEOUT) {
+        if (millis()-down_initial > 20 && input==1) {
+          up_initial = millis();
+          state = UP;
+        }
+      } else {
+        state = START;
+      }
+      break;
+    case UP:
+      if ((millis()-up_initial)< BUTTON_TIMEOUT) {
+        if (millis()-up_initial > 20 && input==0) {
+          down_initial = millis();
+          state = DOWN;
+        }
+      } else {
+        state = START;
+        LED = !LED;
+      }
+      break;
+  }
 }
---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import project 
+import serialcomm 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return '''<!DOCTYPE html>
+    <html>
+    <body>
+      <h1>Lamp Control</h1>
+      <form action="/LED" method="post">
+        <button type="submit" name="submitButton" value="Toggle">Toggle LED</button>
+      </form>
+    </body>
+    </html>'''
+
+@app.route("/LED", methods=['GET', 'POST'])
+def change():
+    serialcomm.writeArduino('2')
+    return '''<!DOCTYPE html>
+    <html>
+    <body>
+      <h1>Lamp Control</h1>
+      <form action="/LED" method="post">
+        <button type="submit" name="submitButton" value="Toggle">Toggle LED</button>
+      </form>
+    </body>
+    </html>'''
+    
+    
+if __name__ == "__main__":
+    app.run(debug=True)
